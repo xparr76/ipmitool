c9831aeeab67e5e586ba726fdb9b64bbe399c8b6
xparr76 <xparr76@gmail.com>
2020-11-22 22:03:48 -0800

    update

--- README.md
+++ README.md
@@ -22,12 +22,18 @@ docker build -t "xparr76/ipmitool" .
 Run docker container
 ```
 docker run -d --name ipmitool \
-	-e IDRACIP=<ip> \
-	-e IDRACUSER=<user> \
-	-e IDRACPASSWORD=<password> \
-	-e TEMPTHRESHOLD=40 \
-	-e FANSPEEDMIN=10 \
-	-e FANSPEEDMAX=90 \
-	-e DELAY=5 \
+	-e IDRACIP=<ip> \								# IP of iDRAC controller
+	-e IDRACUSER=<user> \						# username for iDRAC
+	-e IDRACPASSWORD=<password> \		# password for iDRAC
+	-e TEMPTHRESHOLD=40 \						# At or above temperature in Celsius script takes over from system "dynamic fan control"
+	-e FANSPEEDMIN=20 \							# Minimum % fan speed desired while "dynamic fan control" is disabled.
+	-e FANSPEEDMAX=40 \							# Maximum % fan speed desired while "dynamic fan control" is disabled.
+	-e DELAY=5 \										# Interval between ipmitool request
 	-v ${PWD}:/logs xparr76/ipmitool:latest
+```
+
+Example: Check every 3 seconds, if temp is over 40c, restrict fans speeds to within 20%/40% range.
+```
+docker run -d --name ipmitool -e IDRACIP=192.168.0.50 -e IDRACUSER=myusername -e IDRACPASSWORD=mypassword -e TEMPTH
+RESHOLD=40 -e FANSPEEDMIN=20 -e FANSPEEDMAX=40 -e DELAY=3 -v /logs:/logs xparr76/ipmitool:latest
 ```
--- ipmitool.sh
+++ ipmitool.sh
@@ -12,9 +12,9 @@
 #
 ### Show the sensor output:
 # ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD sdr type Temperature
-# ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD sdr elist full
+# ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD sdr elist full                                # To See Only the Temperature, Voltage, and Fan Sensors
+# ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD sdr elist                                     # All sensors
 # ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD sdr get "Fan1"
-# ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD sdr list
 # ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD sdr type list
 # ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD sdr type Temperature
 # ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD sdr type Fan
@@ -22,12 +22,12 @@
 #
 ### Chassis commands:
 # ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD chassis status
-# ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD chassis status ipmitool chassis identify []	# turn on front panel identify light (default 15s)
-# ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD chassis power soft 				# initiate a soft-shutdown via acpi
-# ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD chassis power cycle 				# issue a hard power off, wait 1s, power on
-# ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD chassis power off 				# issue a hard power off
-# ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD chassis power on 				# issue a hard power on
-# ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD chassis power reset 				# issue a hard reset
+# ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD chassis status ipmitool chassis identify []   # turn on front panel identify light (default 15s)
+# ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD chassis power soft                            # initiate a soft-shutdown via acpi
+# ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD chassis power cycle                           # issue a hard power off, wait 1s, power on
+# ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD chassis power off                             # issue a hard power off
+# ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD chassis power on                              # issue a hard power on
+# ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD chassis power reset                           # issue a hard reset
 #
 ### Modify boot device for the next reboot:
 # ipmitool chassis bootdev pxe
@@ -38,21 +38,14 @@ IDRACIP=${1:-""}
 IDRACUSER=${2:-""}
 IDRACPASSWORD=${3:-""}
 TEMPTHRESHOLD=${4:-"40"}
-FANSPEEDMIN=${5:-"10"}
-FANSPEEDMAX=${6:-"60"}
+FANSPEEDMIN=${5:-"20"}
+FANSPEEDMAX=${6:-"40"}
 DELAY=${7:-"5"}
-
-echo "IDRACIP:" $IDRACIP
-echo "IDRACUSER:" $IDRACUSER
-echo "IDRACPASSWORD:" "******"
-echo "TEMPTHRESHOLD:" $TEMPTHRESHOLD"c"
-echo "FANSPEEDMIN:" $FANSPEEDMIN"%"
-echo "FANSPEEDMAX:" $FANSPEEDMAX"%"
-echo "DELAY: " $DELAY
+FIRSTRUN="true"
 
 int_handler()
 {
-    logInfo "Interrupted"
+    logInfo "WARN" "Interrupted"
 	dynamicFanControl "on"
     # Kill the parent process of the script.
     kill $PPID
@@ -62,48 +55,190 @@ trap 'int_handler' INT
 
 function logInfo() {
 	local TIME=$(date +%Y-%m-%d-%H:%M:%S)
-	local IP=$IDRACIP
-	local MSG=$1
-	echo "$TIME ip:${IP} fanspeed:${CURRENTFANSPEED}rpm temp:${CURRENTTEMP}c last:${LASTTEMP}c ${MSG}" 2>&1 | tee -a /logs/ipmitool_$(date +%Y-%m-%d).log
+  echo $TIME $1 $2 2>&1 | tee -a /logs/ipmitool_$(date +%Y-%m-%d).log
 }
 
-function getCpuTemp() {
-	local SENSORTEMPS=$(ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD sdr type temperature | grep Temp | cut -d"|" -f5 | cut -d" " -f2)
-	IFS=" " read -ra ARRAY <<< "${SENSORTEMPS//$'\n'/ }"
-	# ARRAY[0] = (Inlet Temp)
-	# ARRAY[1] = (Exhaust Temp)
-	# ARRAY[2] = (CPU 1 Temp)
-	# ARRAY[3] = (CPU 2 Temp)
-	# set highest temp cpu to CURRENTTEMP
-	CURRENTTEMP=$(( ${ARRAY[2]} > ${ARRAY[3]} ? ${ARRAY[2]} : ${ARRAY[3]} ))
+function getSensors() {
+  # first run setup
+	if [[ $FIRSTRUN == true ]]; then
+
+    # check for errors
+    if [[ -z "$IDRACIP" ]]; then
+      logInfo "Error" "IP not provided"
+      exit 1
+    elif [[ -z "$IDRACUSER" ]]; then
+      logInfo "Error" "USER not provided"
+      exit 1
+    elif [[ -z "$IDRACPASSWORD" ]]; then
+      logInfo "Error" "PASSWORD not provided"
+      exit 1
+    else
+      # set LASTTEMP and FANSPEED on first run
+  		LASTTEMP=$TOP_CPU_TEMP
+  		FANSPEED=$(($TOP_CPU_TEMP-10))
+      FIRSTRUN=false
+      logInfo "Start"
+      logInfo "ip:${IDRACIP} user:${IDRACUSER} threshold:${TEMPTHRESHOLD} fanmin:${FANSPEEDMIN}% fanmax:${FANSPEEDMAX}% delay:${DELAY}"
+    fi
+	fi
+
+  # call impitool, get all sensors.
+  readarray -t elist < <( ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD sdr elist full | sed 1p )
+  # "sed 1p" dups first element, so we ignore it below [[ $i != 0 ]]
+
+  # reset arrays
+  ARR_FANS=()
+  ARR_INLET_TEMP=()
+  ARR_EXHAUST_TEMP=()
+  ARR_AMPS=()
+  ARR_VOLTAGE=()
+  ARR_WATTS=()
+  ARR_CPU_TEMP=()
+
+  for i in "${!elist[@]}"; do
+    if [[ $i != 0 ]] && [[ ${elist[i]} == *"Fan"* ]]; then
+      # echo "ARR_FANS:" ${elist[i]}
+      IFS='|' read -r -a array <<< $(echo ${elist[i]} | sed 's/ //g' | sed 's/RPM//g')
+      ARR_FANS+=("${array[4]}")
+
+    elif [[ $i != 0 ]] && [[ ${elist[i]} == *"Inlet Temp"* ]]; then
+      # echo "ARR_INLET_TEMP:" ${elist[i]}
+      IFS='|' read -r -a array <<< $(echo ${elist[i]} | sed 's/ //g' | sed 's/degreesC//g')
+      ARR_INLET_TEMP+=("${array[4]}")
+      STRING_INLET_TEMP=$STRING_INLET_TEMP"InletTemp${i}:"${array[4]}"c "
+
+    elif [[ $i != 0 ]] && [[ ${elist[i]} == *"Exhaust Temp"* ]]; then
+      # echo "ARR_EXHAUST_TEMP:" ${elist[i]}
+      IFS='|' read -r -a array <<< $(echo ${elist[i]} | sed 's/ //g' | sed 's/degreesC//g')
+      ARR_EXHAUST_TEMP+=("${array[4]}")
+      STRING_EXHAUST_TEMP=$STRING_EXHAUST_TEMP$"ExhaustTemp${i}:"${array[4]}"c "
+
+    elif [[ $i != 0 ]] && [[ ${elist[i]} == *"Amps"* ]]; then
+      # echo "ARR_AMPS:" ${elist[i]}
+      IFS='|' read -r -a array <<< $(echo ${elist[i]} | sed 's/ //g' | sed 's/Amps//g')
+      ARR_AMPS+=("${array[4]}")
+      STRING_AMPS=$STRING_AMPS"Amps${i}:"${array[4]}" "
+
+    elif [[ $i != 0 ]] && [[ ${elist[i]} == *"Volts"* ]]; then
+      # echo "ARR_VOLTAGE:" ${elist[i]}
+      IFS='|' read -r -a array <<< $(echo ${elist[i]} | sed 's/ //g' | sed 's/Volts//g')
+      ARR_VOLTAGE+=("${array[4]}")
+      STRING_VOLTS=$STRING_VOLTS"Volts${i}:"${array[4]}" "
+
+    elif [[ $i != 0 ]] && [[ ${elist[i]} == *"Watts"* ]]; then
+      # echo "ARR_WATTS:" ${elist[i]}
+      IFS='|' read -r -a array <<< $(echo ${elist[i]} | sed 's/ //g' | sed 's/Watts//g')
+      ARR_WATTS+=("${array[4]}")
+      STRING_WATTS=$STRING_WATTS"Watts${i}:"${array[4]}" "
+
+    elif [[ $i != 0 ]] && [[ ${elist[i]} == *"Temp"* ]] && [[ ${elist[i]} != *"Inlet"* ]] && [[ ${elist[i]} != *"Exhaust"* ]]; then
+      # echo "ARR_CPU_TEMP:" ${elist[i]}
+      IFS='|' read -r -a array <<< $(echo ${elist[i]} | sed 's/ //g' | sed 's/degreesC//g')
+      ARR_CPU_TEMP+=("${array[4]}")
+
+    fi
+  done
+
+  # capture top fan speed
+  local log_fans
+  for i in "${!ARR_FANS[@]}"; do
+   if [[ ${ARR_FANS[i]} =~ ^[-+]?[0-9]+\.?[0-9]*$ ]];then
+     local x=$((i+1))
+     if [[ $x -le ${#ARR_FANS[*]} ]]; then
+       if [[ ${ARR_FANS[i]} > ${ARR_FANS[x]} ]]; then
+         TOP_FAN_SPEED=${ARR_FANS[i]}
+       else
+         TOP_FAN_SPEED=${ARR_FANS[x]}
+       fi
+       log_fans=$log_fans"FAN$x:"${ARR_FANS[i]}"rpm "
+     fi
+   fi
+  done
+
+  # capture inlet temp
+  local log_inlet_temp
+  for i in "${!ARR_INLET_TEMP[@]}"; do
+   if [[ ${ARR_INLET_TEMP[i]} =~ ^[-+]?[0-9]+\.?[0-9]*$ ]];then
+     local x=$((i+1))
+     if [[ $x -le ${#ARR_INLET_TEMP[*]} ]]; then
+       log_inlet_temp=$log_inlet_temp"INLET"$x"TEMP:"${ARR_INLET_TEMP[i]}"c "
+     fi
+   fi
+  done
+
+  # capture exhaust temp
+  local log_exhaust_temp
+  for i in "${!ARR_EXHAUST_TEMP[@]}"; do
+   if [[ ${ARR_EXHAUST_TEMP[i]} =~ ^[-+]?[0-9]+\.?[0-9]*$ ]];then
+     local x=$((i+1))
+     if [[ $x -le ${#ARR_EXHAUST_TEMP[*]} ]]; then
+       log_exhaust_temp=$log_exhaust_temp"EXHAUST"$x"TEMP:"${ARR_EXHAUST_TEMP[i]}"c "
+     fi
+   fi
+  done
+
+  # capture amps
+  local log_amps
+  for i in "${!ARR_AMPS[@]}"; do
+   if [[ ${ARR_AMPS[i]} =~ ^[-+]?[0-9]+\.?[0-9]*$ ]];then
+     local x=$((i+1))
+     if [[ $x -le ${#ARR_AMPS[*]} ]]; then
+       log_amps=$log_amps"CURRENT$x:"${ARR_AMPS[i]}" "
+     fi
+   fi
+  done
+
+  # capture volts
+  local log_voltage
+  for i in "${!ARR_VOLTAGE[@]}"; do
+   if [[ ${ARR_VOLTAGE[i]} =~ ^[-+]?[0-9]+\.?[0-9]*$ ]];then
+     local x=$((i+1))
+     if [[ $x -le ${#ARR_VOLTAGE[*]} ]]; then
+       log_voltage=$log_voltage"VOLTAGE$x:"${ARR_VOLTAGE[i]}"v "
+     fi
+   fi
+  done
+
+  # capture watts
+  local log_watts
+  for i in "${!ARR_WATTS[@]}"; do
+   if [[ ${ARR_WATTS[i]} =~ ^[-+]?[0-9]+\.?[0-9]*$ ]];then
+     local x=$((i+1))
+     if [[ $x -le ${#ARR_WATTS[*]} ]]; then
+       log_watts=$log_watts"WATTS$x:"${ARR_WATTS[i]}" "
+     fi
+   fi
+  done
+
+  # capture cpu temp
+  local log_cpu_temp
+  for i in "${!ARR_CPU_TEMP[@]}"; do
+    if [[ ${ARR_CPU_TEMP[i]} =~ ^[-+]?[0-9]+\.?[0-9]*$ ]];then
+      local x=$((i+1))
+      if [[ $x -le ${#ARR_CPU_TEMP[*]} ]]; then
+        if [[ ${ARR_CPU_TEMP[i]} > ${ARR_CPU_TEMP[x]} ]]; then
+          TOP_CPU_TEMP=${ARR_CPU_TEMP[i]}
+        else
+          TOP_CPU_TEMP=${ARR_CPU_TEMP[x]}
+        fi
+        log_cpu_temp=$log_cpu_temp" CPU"$x"TEMP:"${ARR_CPU_TEMP[i]}"c "
+      fi
+    fi
+  done
+
+  logInfo "INFO" "${log_fans}  ${log_inlet_temp}  ${log_exhaust_temp}  ${log_amps} ${log_voltage}  ${log_watts}  ${log_cpu_temp}"
 }
 
-function getFanSpeed() {
-	local SENSORFANS=$(ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD sdr type fan | grep Fan | cut -d"|" -f5 | cut -d" " -f2)
-	IFS=" " read -ra FANARRAY <<< "${SENSORFANS//$'\n'/ }"
-	# FANARRAY[0] = (Fan1)
-	# FANARRAY[1] = (Fan2)
-	# FANARRAY[2] = (Fan3)
-	# FANARRAY[3] = (Fan4)
-	# FANARRAY[4] = (Fan5)
-	# FANARRAY[5] = (Fan6)
-	# set highest speed fan to CURRENTFANSPEED
-	CURRENTFANSPEED=$(( ${FANARRAY[0]} > ${FANARRAY[1]} ? ${FANARRAY[0]} : ${FANARRAY[1]} ))
-	CURRENTFANSPEED=$(( ${FANARRAY[1]} > ${FANARRAY[2]} ? ${FANARRAY[1]} : ${FANARRAY[2]} ))
-	CURRENTFANSPEED=$(( ${FANARRAY[2]} > ${FANARRAY[3]} ? ${FANARRAY[2]} : ${FANARRAY[3]} ))
-	CURRENTFANSPEED=$(( ${FANARRAY[3]} > ${FANARRAY[4]} ? ${FANARRAY[3]} : ${FANARRAY[4]} ))
-	CURRENTFANSPEED=$(( ${FANARRAY[4]} > ${FANARRAY[5]} ? ${FANARRAY[4]} : ${FANARRAY[5]} ))
-}
 
 function dynamicFanControl(){
 	FANCONTROL=${1:-on}
-	if [[ $FANCONTROL = "on" ]] && [[ $LASTDYNFAN != "on" ]]; then
-		LASTDYNFAN="on"
-		logInfo "enable dynamic fan control"
+	if [[ $FANCONTROL = "on" ]] && [[ $LASTFANCONTROLVALUE != "on" ]]; then
+		LASTFANCONTROLVALUE="on"
+		logInfo "INFO" "enable dynamic fan control"
 		ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD raw 0x30 0x30 0x01 0x01
-	elif [[ $FANCONTROL = "off" ]] && [[ $LASTDYNFAN != "off" ]]; then
-		LASTDYNFAN="off"
-		logInfo "disable dynamic fan control"
+	elif [[ $FANCONTROL = "off" ]] && [[ $LASTFANCONTROLVALUE != "off" ]]; then
+		LASTFANCONTROLVALUE="off"
+		logInfo "INFO" "disable dynamic fan control"
 		ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD raw 0x30 0x30 0x01 0x00
 	fi
 }
@@ -111,57 +246,41 @@ function dynamicFanControl(){
 function changeFanSpeed(){
 	# convert fan speed value from dec to hex
 	local HEXVALUE=$(printf "%x" $1)
+  # set fan speed %
 	ipmitool -I lanplus -H $IDRACIP -U $IDRACUSER -P $IDRACPASSWORD raw 0x30 0x30 0x02 0xff 0x$HEXVALUE
-}
-
-function fanUp(){
-	FANSPEED=$(( ( $FANSPEED + 1 ) + ( $CURRENTTEMP - $LASTTEMP ) + ( ( $CURRENTTEMP - $TEMPTHRESHOLD ) / 5 ) ))
-	changeFanSpeed $FANSPEED
-}
-
-function fanDown(){
-	FANSPEED=$(( ( $FANSPEED - 1 ) - ( $LASTTEMP - $CURRENTTEMP )))
-	changeFanSpeed $FANSPEED
 }
 
 while true
 do
-	# call ipmitool and return temps
-	getCpuTemp
-	getFanSpeed
-
-	# set LASTTEMP and FANSPEED on first run
-	if [[ $LASTTEMP < 1 ]]; then
-		LASTTEMP=$CURRENTTEMP
-		FANSPEED=$(($CURRENTTEMP-10))
-		logInfo "Start"
-	fi
+	# call ipmitool and return temperature & fanspeed
+	getSensors
 
 	# only control fan speed if highest cpu temp is higher then threshold
-	if [[ $CURRENTTEMP > $TEMPTHRESHOLD ]]; then
+	if [[ $TOP_CPU_TEMP > $TEMPTHRESHOLD ]]; then
 		# disable dynamic fan control
 		dynamicFanControl "off"
 
 		# Increase or decrease a fan speed to match cpu temp changes
-		if [[ $FANSPEED < $FANSPEEDMAX && $CURRENTTEMP > $LASTTEMP ]]; then
-			logInfo "speed up fan"
-			fanUp
+		if [[ $FANSPEED < $FANSPEEDMAX && $TOP_CPU_TEMP > $LASTTEMP ]]; then
+      changeFanSpeed $((FANSPEED+1))
+      logInfo "INFO" "speed up fan"
 
-		elif [[ $FANSPEED > $FANSPEEDMIN && $CURRENTTEMP < $LASTTEMP ]]; then
-			logInfo "slow down fan"
-			fanDown
+		elif [[ $FANSPEED > $FANSPEEDMIN && $TOP_CPU_TEMP < $LASTTEMP ]]; then
+      changeFanSpeed $((FANSPEED-1))
+			logInfo "INFO" "slow down fan"
 
 		else
-			logInfo "idle no change"
+			logInfo "INFO" "idle no change"
+
 		fi
 	else
-		LASTTEMP=$CURRENTTEMP
-		FANSPEED=$(($CURRENTTEMP-10))
+    # reset fan speed value
+		FANSPEED=$(($TOP_CPU_TEMP-10))
 
 		# enable dynamic fan control
-		logInfo "idle dynamic state"
 		dynamicFanControl "on"
+    logInfo "INFO" "idle dynamic state"
 	fi
-	LASTTEMP=$CURRENTTEMP
+	LASTTEMP=$TOP_CPU_TEMP
 	sleep $DELAY
 done
